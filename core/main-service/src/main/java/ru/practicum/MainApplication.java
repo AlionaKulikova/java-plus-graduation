8c98359c15f2b6550e1280cad2c7ede644e21b0c
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2025-07-19 22:12:31 +0300

    подготоаила проект к запуску в облачных средах

---
+++ core/main-service/src/main/java/ru/practicum/MainApplication.java
@@ -0,0 +1,17 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication(scanBasePackages = {"ru.practicum", "ewm.client"})
+@EnableDiscoveryClient
+@ConfigurationPropertiesScan
+@EnableFeignClients(basePackages = "ewm.client")  // Explicitly specify Feign clients package
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
